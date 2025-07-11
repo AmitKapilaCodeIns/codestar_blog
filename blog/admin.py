280c7f106982c0cd41f72a448fbfdbe1df414624
AmitKapilaCodeIns <amitkapila@hotmail.com>
2025-07-11 13:30:14 +0100

    commented out variables from base.html

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,19 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+"""
+The decorator is how we register a class, compared to just
+registering the standard model as on line 20. When we use a class,
+we register it with a decorator which is more Pythonic and allows
+us to customize how the models we are registering will appear
+in the admin interface.
+
+You need to run:
+
+python3 manage.py migrate
+
+after installing django-summernote because it comes with its own database models that need to be added to your database.
+"""
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
@@ -14,3 +27,5 @@ class PostAdmin(SummernoteModelAdmin):
 
 # Register your models here.
 admin.site.register(Comment)
+# We can remove this line "admin.site.register(Post)" 
+# after using the decorator above.
